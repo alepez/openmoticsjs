caf2b6eea0b4e6a40d37ab5d751982495cdb6fbe
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2016-01-03 22:27:21 +0100

    other misc functions without parameters

--- test/test.js
+++ test/test.js
@@ -2,7 +2,6 @@ var assert = require('assert');
 var OpenMoticsApi = require('../lib/index.js').OpenMoticsApi;
 
 describe('OpenMoticsApi', function() {
-
   /* to test a real OpenMotics gateway, we must provide these variables: */
   var env = {
     username: process.env.OPENMOTICS_USERNAME,
@@ -11,9 +10,14 @@ describe('OpenMoticsApi', function() {
     port: process.env.OPENMOTICS_PORT || 443
   };
 
+  var api = OpenMoticsApi(env.username, env.password, env.hostname, false, env.port);
+
+  var handleError = function(done) {
+    throw new Error("Error!");
+  }
+
   describe('init()', function() {
-    it('initialize', function() {
-      var api = OpenMoticsApi(env.username, env.password, env.hostname, false, env.port);
+    it('should initialize correctly', function() {
       assert.equal(api.auth.username, env.username);
       assert.equal(api.auth.password, env.password);
     });
@@ -21,15 +25,12 @@ describe('OpenMoticsApi', function() {
 
   describe('get_url()', function() {
     it('should return a valid url', function() {
-      var api = OpenMoticsApi(env.username, env.password, env.hostname, false, env.port);
       assert.equal(api.get_url('action'), 'https://' + env.hostname + ':443/action');
     });
   });
 
   describe('login()', function() {
     it('should get a token', function(done) {
-      var api = OpenMoticsApi(env.username, env.password, env.hostname, false, env.port);
-
       api.login().then(function() {
         assert.equal(typeof api.token, 'string');
         assert.equal(api.token.length, 32);
@@ -38,21 +39,28 @@ describe('OpenMoticsApi', function() {
     });
   });
 
-  describe('authenticated', function() {
-    var api = OpenMoticsApi(env.username, env.password, env.hostname, false, env.port);
-
-    describe('get_version', function() {
-      it('should get version', function(done) {
-        api.get_version().then(function(res) {
-          var version = res['version'];
-          var versionArr = version.split('.');
-          /*  semantic version has major.minor.patch */
-          assert.equal(versionArr.length, 3);
-          /* this should work with major version 1 */
-          assert.equal(versionArr[0], 1);
-          done();
-        }).catch(done);
-      });
+  describe('get_version()', function() {
+    it('should get version', function(done) {
+      api.get_version().then(function(res) {
+        assert.equal(res['success'], true);
+        var version = res['version'];
+        var versionArr = version.split('.');
+        /*  semantic version has major.minor.patch */
+        assert.equal(versionArr.length, 3);
+        /* this should work with major version 1 */
+        assert.equal(versionArr[0], 1);
+        done();
+      }).catch(done);
     });
   });
+
+  describe('get_status()', function() {
+    it('should get status', function(done) {
+      api.get_status().then(function(res) {
+        assert.equal(res['success'], true);
+        done();
+      }).catch(done);
+    });
+  });
+
 });
