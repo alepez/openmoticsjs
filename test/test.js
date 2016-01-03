dc784b9a874214e49504cffa8ddc6dc50d92efd2
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2016-01-03 20:30:38 +0100

    fetch_url and login

--- test/test.js
+++ test/test.js
@@ -3,27 +3,40 @@ var OpenMoticsApi = require('../lib/index.js').OpenMoticsApi;
 
 describe('OpenMoticsApi', function() {
 
-  describe('#init', function() {
+  /* to test a real OpenMotics gateway, we must provide these variables: */
+  var env = {
+    username: process.env.OPENMOTICS_USERNAME,
+    password: process.env.OPENMOTICS_PASSWORD,
+    hostname: process.env.OPENMOTICS_HOSTNAME || 'localhost',
+    port: process.env.OPENMOTICS_PORT || 443
+  };
+
+  describe('init()', function() {
     it('initialize', function() {
-      var api = OpenMoticsApi('foo', 'bar', 'localhost', false, 443);
-      assert(api.auth.username === 'foo');
-      assert(api.auth.password === 'bar');
+      var api = OpenMoticsApi(env.username, env.password, env.hostname, false, env.port);
+      assert.equal(api.auth.username, env.username);
+      assert.equal(api.auth.password, env.password);
     });
   });
 
-  describe('get_url', function() {
+  describe('get_url()', function() {
     it('should return a valid url', function() {
-      var api = OpenMoticsApi('foo', 'bar', 'localhost', false, 443);
-      assert(api.get_url('action') === 'https://localhost:443/action');
+      var api = OpenMoticsApi(env.username, env.password, env.hostname, false, env.port);
+      assert.equal(api.get_url('action'), 'https://' + env.hostname + ':443/action');
     });
   });
 
-  // describe('#login()', function() {
-  //   it('should get a token', function() {
-  //     var api = OpenMoticsApi('foo', 'bar', 'localhost', false, 443);
-  //     api.login().then(function() {
-  //       assert(api.login);
-  //     })
-  //   });
-  // })
+  describe('login()', function() {
+    it('should get a token', function(done) {
+      var api = OpenMoticsApi(env.username, env.password, env.hostname, false, env.port);
+      api.login().then(function() {
+        // assert(false);
+        // assert(typeof api.token === 'string');
+        console.log(api.token);
+        done();
+      }).fail(function() {
+        done('error');
+      })
+    });
+  })
 });
