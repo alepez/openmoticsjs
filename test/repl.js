de90130cdc25bd872553b1a6292e88f59773dde1
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2016-01-04 01:30:47 +0100

    minor fixes

---
+++ test/repl.js
@@ -0,0 +1,10 @@
+var OpenMoticsApi = require('../lib/index.js').OpenMoticsApi;
+
+var env = {
+    username: process.env.OPENMOTICS_USERNAME,
+    password: process.env.OPENMOTICS_PASSWORD,
+    hostname: process.env.OPENMOTICS_HOSTNAME || 'localhost',
+    port: process.env.OPENMOTICS_PORT || 443
+};
+
+module.exports = OpenMoticsApi(env.username, env.password, env.hostname, false, env.port);
